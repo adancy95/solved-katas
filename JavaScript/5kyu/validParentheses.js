30b81df554ef9467659db52b4ffbae9477307e38
Antquanique Dancy <antdancy95@gmail.com>
2019-11-24 21:12:05 -0500

    added another best solution

--- JavaScript/5kyu/validParentheses.js
+++ JavaScript/5kyu/validParentheses.js
@@ -34,3 +34,16 @@ function validParentheses(string){
     }
     return count == 0;
  }
+
+//  another solution
+
+ function validParentheses(parens){
+    var n = 0;
+    for (var i = 0; i < parens.length; i++) {
+      if (parens[i] == '(') n++;
+      if (parens[i] == ')') n--;
+      if (n < 0) return false;
+    }
+    
+    return n == 0;
+  }
