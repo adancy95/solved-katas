b9b4189647ffe71689ca8ac28966565d865eae85
Ant-Quanique Dancy <antdancy95@gmail.com>
2020-02-06 11:48:36 -0500

    solved reversed String kata

---
+++ JavaScript/8kyu/reversedStrings.js
@@ -0,0 +1,8 @@
+//Reversed Strings
+
+// Complete the solution so that it reverses the string value passed into it.
+
+// solution('world'); // returns 'dlrow'
+
+//My Solution
+const solution = str => str.split("").reverse().join("");
